6acaf5ddc1c0ee9225f7d4ac3ac4a63f94eaff4e
tom <258146@whut.edu.cn>
2022-02-16 10:19:06 +0800

    2022-02-16

--- study_record/urls.py
+++ study_record/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('read', ReadNoteListView.as_view()),
-    path('read/<int:id>/detail', ReadNoteDetailView.as_view()),
-    path('read/<int:id>/edit', ReadNoteEditView.as_view())
+    path('read', ReadNoteListView.as_view(), name='read_list'),
+    path('read/<int:id>/detail', ReadNoteDetailView.as_view(), name='read_detail'),
+    path('read/<int:id>/edit', ReadNoteEditView.as_view(), name='read_edit'),
+    path('read/<int:id>/delete', ReadNoteDeleteView.as_view(), name='read_delete')
 ]
--- study_record/views.py
+++ study_record/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.views.generic import ListView, DeleteView, DetailView
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, CreateView
 from .models import ReadNoteModel
-from .forms import ReadNoteForm
+from django.urls import reverse_lazy
+# from .forms import ReadNoteForm
 
 
 class ReadNoteListView(ListView):
@@ -21,8 +22,37 @@ class ReadNoteDetailView(DetailView):
 
 class ReadNoteEditView(UpdateView):
     model = ReadNoteModel
+    context_object_name = "read_note_edit"
     template_name = "study_record/read_note.html"
-    context_object_name = "read_note_edit_form"
     fields = ['title', 'content', 'link']
     pk_url_kwarg = "id"
 
+    def get_context_data(self, **kwargs):
+        context = super(ReadNoteEditView, self).get_context_data(**kwargs)
+        context['read_note_edit_form'] = context.get('form')
+        return context
+
+
+class ReadNoteDeleteView(DeleteView):
+    model = ReadNoteModel
+    success_url = reverse_lazy('read_list')
+    pk_url_kwarg = 'id'
+    context_object_name = "read_note_delete"
+    template_name = "study_record/read_note.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ReadNoteDeleteView, self).get_context_data(**kwargs)
+        context['read_note_delete_form'] = context.get('form')
+        return context
+
+
+class ReadNoteCreateView(CreateView):
+    model = ReadNoteModel
+    template_name = "study_record/read_note.html"
+    context_object_name = "read_note_create"
+    fields = ['title', 'content', 'link']
+
+    def get_context_data(self, **kwargs):
+        context = super(ReadNoteCreateView, self).get_context_data(**kwargs)
+        context['read_note_create_form'] = context.get('form')
+        return context
